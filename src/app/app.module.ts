aa9c59111957874bc0a58bbc3d3ac8b2abd1287b
Daniel Fischer <lrcf813@grz.rbgooe.at>
2021-07-14 14:56:41 +0200

    Display simple slider and grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import { PersonGridComponent } from './person-grid/person-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
