b12211f655cc63aa71d1734a7eb9a4d09fd82b4e
Daniel Fischer <lrcf813@grz.rbgooe.at>
2021-07-14 15:37:00 +0200

    Simple grid with person data

--- src/app/person-grid/person-grid.component.ts
+++ src/app/person-grid/person-grid.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  lastname: string;
+  firstname: string;
+  age: number;
+  address: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {lastname: "Huber", firstname: "Franz", age: 32, address: "Rosengarten 12"},
+  {lastname: "Müller", firstname: "Jonas", age: 21, address: "Schulstraße 3"},
+  {lastname: "Schmidt", firstname: "Lukas", age: 45, address: "Ziegeleistraße 5"},
+  {lastname: "Fischer", firstname: "Julian", age: 51, address: "Bäckergasse 22"},
+  {lastname: "Wagner", firstname: "Thomas", age: 75, address: "Wintersberg 17"},
+];
+
 @Component({
   selector: 'app-person-grid',
   templateUrl: './person-grid.component.html',
@@ -12,4 +27,7 @@ export class PersonGridComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+  displayedColumns: string[] = ['lastname', 'firstname', 'age', 'address'];
+  dataSource = ELEMENT_DATA;
 }
